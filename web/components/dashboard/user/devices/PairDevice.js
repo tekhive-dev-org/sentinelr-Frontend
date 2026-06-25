adae0dfc4b3bb8071a10044f832d5ae26d8c6903
codemantix <codemantixcollective@gmail.com>
2026-06-25 16:23:12 +0100

    fix: corrected casing of 'iOS' to 'IOS' in PairDevice component

--- web/components/dashboard/user/devices/PairDevice.js
+++ web/components/dashboard/user/devices/PairDevice.js
@@ -164,7 +164,7 @@ export default function PairDevice({
       const platformValue =
         deviceType === "Phone"
           ? smartphoneOS === "ios"
-            ? "iOS"
+            ? "IOS"
             : "Android"
           : null;
 
